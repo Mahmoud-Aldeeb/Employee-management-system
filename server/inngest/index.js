fd94adf49822b3000a2bdbd56d297805f20f759d
Mahmoud-Aldeeb <145771735+Mahmoud-Aldeeb@users.noreply.github.com>
2026-07-04 06:36:03 +0300

    update inngest

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -96,7 +96,7 @@ const leaveApplicationReminder = inngest.createFunction(
 
 // Cron: Check attendance at 11:30 AM IST (06:00 UTC) and email absent employees
 const attendanceReminderCron = inngest.createFunction(
-  { id: "attendance-reminder-cron", triggers: [{ cron: "0 0 6 * * *" }] }, //06:00 UTC = 8:00 AM EGY
+  { id: "attendance-reminder-cron", triggers: [{ cron: "0 6 * * *" }] }, //06:00 UTC = 8:00 AM EGY
 
   // async ({ step }) => {
   //   // Step 1: Get today's date range (IST)
